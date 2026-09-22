88df48e25a1333e381bd9a3fa39cf34bea02a405
kaushikmayank12p-source <kaushikmayank12p@gmail.com>
2026-09-22 08:33:25 +0000

    "blabla "

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ const mediaExplanation = document.getElementById('media-explanation');
 // Set maximum date to today so users can't pick future dates
 const today = new Date().toISOString().split('T')[0];
 datePicker.max = today;
-datePicker.value = today;
-
+datePicker.value = today;hackatim
 async function fetchAPOD(date = '') {
   loader.classList.remove('hidden');
   mediaContainer.innerHTML = '';
